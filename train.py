13c21e2a94d3022ddce2f1869aa367c66a81aa0f
VYPang <121414357+VYPang@users.noreply.github.com>
2024-07-01 01:29:08 +0100

    [New Feature] enable semi-supervised learning

--- train.py
+++ train.py
@@ -5,12 +5,17 @@ from data.dataLoading import trainSource
 from torch.utils.data import DataLoader
 from utils.model import AlexNet
 import torch.optim as optim
-import numpy as np
 import datetime
 from omegaconf import OmegaConf
+import numpy as np
 import os
 
-def train(savePath, model, optimizer, lossFunction, config, trainLoader, valLoader=None):
+def train(savePath, device, lossFunction, config, trainLoader, valLoader=None, saveModel=True):
+    numClass = len(config.data.classes)
+    channel = config.data.shape[0]
+    model = AlexNet(channel, numClass).to(device)
+    optimizer = optim.SGD(model.parameters(), lr=config.train.lr, momentum=config.train.momentum)
+
     epochs = config.train.epochs
     for epoch in range(epochs):
         # training loop
@@ -50,15 +55,46 @@ def train(savePath, model, optimizer, lossFunction, config, trainLoader, valLoad
                 total_val_loss += loss
                 average_val_loss = round((total_val_loss.detach().cpu().numpy()/ (batch_idx + 1)), 5)
                 val_tqdm.set_postfix(val_loss=average_val_loss)
-        print('\n')
         
-        if epoch+1 % 20 == 0:
+        if epoch+1 % 20 == 0 and saveModel:
             torch.save(model, savePath + f'/epoch{epoch+1}-{average_loss}-{average_val_loss}.pt')
-    torch.save(model.state_dict(), savePath + f'/final.pt')
-    print('Model saved, all done!')
+    if saveModel:
+        torch.save(model.state_dict(), savePath + f'/final.pt')
+    else:
+        return model
 
-def inference(model, lossFunction, config, trainLoader):
-    pass
+def inference(model, lossFunction, config, dataSets):
+    dataLoaderList = [DataLoader(data, batch_size=config.train.batch_size, shuffle=False) for data in dataSets]
+    model.eval()
+    for i in tqdm(range(len(dataLoaderList)), desc='Inference'):
+        dataLoader = dataLoaderList[i]
+        dataSet = dataSets[i]
+        for batch_idx, batch in enumerate(dataLoader):
+            x, y, mainSet_idx = batch
+            y = y.to(device)
+            x = x.float().to(device)
+            if len(x.shape) == 3:
+                x = x[:, None, ...]
+            output = model(x)
+            _, predicted = torch.max(output, 1)
+            dataSet.updateLabel(mainSet_idx.cpu().numpy(), predicted.cpu().numpy())
+
+def semiSupervisedLearning(savePath, device, lossFunction, config, labeledSet, unlabeledSet, valLoader=None):
+    # Train model with labeled data
+    print('Training with labeled data')
+    labeledLoader = DataLoader(labeledSet, batch_size=config.train.batch_size, shuffle=True)
+    model = train(savePath, device, lossFunction, config, labeledLoader, valLoader, saveModel=False)
+
+    # Update pseudo-label of unlabeled data with model
+    print('Inference with unlabeled data')
+    inference(model, lossFunction, config, unlabeledSet)
+    
+    # Train model with labeled and pseudo-labeled data
+    print('Training with labeled and pseudo-labeled data')
+    mixedData = torch.utils.data.ConcatDataset([labeledSet] + unlabeledSet)
+    mixedLoader = DataLoader(mixedData, batch_size=config.train.batch_size, shuffle=True)
+    train(savePath, device, lossFunction, config, mixedLoader, valLoader, saveModel=True)
+    print('Training completed, model saved at', savePath)
 
 if __name__ == "__main__":
     configPath = 'configuration/config.yaml'
@@ -77,18 +113,16 @@ if __name__ == "__main__":
     os.makedirs(savePath)
 
     # load dataset
-    trainSet = trainSource(numGroups=numGroups, valSplit=config.train.val_split, augmentation=True)
-    trainLoader = [DataLoader(i, batch_size=config.train.batch_size, shuffle=True) for i in trainSet.groups]
+    trainData = trainSource(numGroups=numGroups, valSplit=config.train.val_split, augmentation=True)
+    labeledData = trainData.labeledSet
+    unlabeledData = trainData.unlabeledSet
+
     if config.train.val_split > 0:
-        valLoader = DataLoader(trainSet.valSet, batch_size=config.train.batch_size, shuffle=True)
+        valLoader = DataLoader(trainData.valSet, batch_size=config.train.batch_size, shuffle=True)
     else:
         valLoader = None
-
-    numClass = len(config.data.classes)
-    channel = config.data.shape[0]
-    model = AlexNet(channel, numClass).to(device)
-    optimizer = optim.SGD(model.parameters(), lr=config.train.lr, momentum=config.train.momentum)
+    
     lossFunction = nn.CrossEntropyLoss()
-
-    # train
-    train(savePath, model, optimizer, lossFunction, config, trainLoader[0], valLoader)
+    # semiSupervisedLearning(savePath, device, lossFunction, config, labeledData, unlabeledData, valLoader)
+    trainLoader = DataLoader(labeledData, batch_size=config.train.batch_size, shuffle=True)
+    train(savePath, device, lossFunction, config, trainLoader, valLoader=valLoader, saveModel=True)
